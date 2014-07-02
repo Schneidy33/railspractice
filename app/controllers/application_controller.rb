5ffa91c4325e6260131f1466eb3bda19fa953191
Your Name <you@example.com>
2014-07-02 16:04:03 +0000

    final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,38 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def show
+    @ideas= Ideas.find_by_id(params['id'])
+  end
+  
+  def new
+  end
+  
+  def create
+    i= Ideas.new
+    i.text= params['text']
+    i.save
+    redirect_to"/ideas/#{i.id}"
+  end
+  
+  def edit
+     @ideas= Ideas.find_by_id(params['id'])
+  end
+  
+  def update
+    i= Ideas.find_by_id(params['id'])
+    i.text= params['text']
+    i.save
+    redirect_to"/ideas/#{i.id}"
+  end
+  
+  def destroy
+    i= Ideas.find_by_id(params['id'])
+    i.destroy
+    redirect_to "/ideas"
+  end
+  
+  def index
+    @ideas= Ideas.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
- 
-  do show
-  end
+ get'/ideas/:id' => 'application#show'
+  get'/new_idea' => 'application#new'
+  get'/create_idea'=> 'application#create'
+  get'/update_idea/:id' => 'application#update'
+  get'/ideas/:id/destroy' => 'application#destroy'
+  get'/ideas' => 'application#index'
 end
